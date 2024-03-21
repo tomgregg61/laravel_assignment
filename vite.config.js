f8166ca0668c195cd341c4381185651e5ba9e0fc
CptFlashbang~ <Callum.yates.180@gmail.com>
2024-03-21 10:08:57 +0000

    Added usePolling: true to check for files changes

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,16 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ['resources/scss/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+        watch: {
+            usePolling: true,
+        }
+    },
 });
