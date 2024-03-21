79765fd4d09dda412b5e72534ba59b3c6108b4a1
CptFlashbang~ <Callum.yates.180@gmail.com>
2024-03-21 11:41:38 +0000

    added validation to chirp store function

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -1,6 +1,7 @@
 <?php
-
 namespace App\Http\Controllers;
+
+use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
 use App\Models\Chirp;
 use Illuminate\Http\Request;
@@ -27,10 +28,18 @@ class ChirpController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
+    public function store(Request $request): RedirectResponse
+   {
         //
-    }
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+ 
+        $request->user()->chirps()->create($validated);
+ 
+        return redirect(route('chirps.index'));
+   }
+
 
     /**
      * Display the specified resource.
