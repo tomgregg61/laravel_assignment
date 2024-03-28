3d441d52d22a0f90d88f4054313048fff30778ce
CptFlashbang~ <Callum.yates.180@gmail.com>
2024-03-28 11:56:38 +0000

    added edited tag to chirp

--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -21,6 +21,9 @@
                             <div>
                                 <span class="text-gray-800">{{ $chirp->user->name }}</span>
                                 <small class="ml-2 text-sm text-gray-600">{{ $chirp->created_at->format('j M Y, g:i a') }}</small>
+                                @unless ($chirp->created_at->eq($chirp->updated_at))
+                                    <small class="text-sm text-gray-600"> &middot; {{ __('edited') }}</small>
+                                @endunless
                             </div>
                         </div>
                         <p class="mt-4 text-lg text-gray-900">{{ $chirp->message }}</p>
